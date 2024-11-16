d0e543f1d2e0899bef6b506c7454ba4658014161
glince21 <glince21@uan.edu.co>
2024-11-15 19:48:59 -0500

    Delete inventoryapp/inventoryapp directory

--- inventoryapp/inventoryapp/app/src/main/java/com/example/inventoryapp/ui/Product.kt
+++
@@ -1,8 +0,0 @@
-package com.example.inventoryapp
-
-data class Product(
-    var code: String = "",
-    var name: String = "",
-    var quantity: Int = 0,
-    var category: String = ""
-)
--- inventoryapp/inventoryapp/app/src/main/java/com/example/inventoryapp/ui/Sale.kt
+++
@@ -1,9 +0,0 @@
-package com.example.inventoryapp
-
-import java.util.Date
-
-data class Sale(
-    val productCode: String = "",
-    val quantity: Int = 0,
-    val date: String = "" // Podrías usar un tipo `Date` si prefieres trabajar con fechas
-)
